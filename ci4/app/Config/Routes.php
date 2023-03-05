57d5e34a076c2ceb362639ad84d72b63c3791c1e
Lyka Tesorero <lctesorero@student.apc.edu.ph>
2023-03-05 20:55:34 +0800

    created news item

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -32,9 +32,14 @@ $routes->set404Override();
 $routes->get('/', 'Home::index');
 
 
-
+use App\Controllers\Guest;
 use App\Controllers\News;
 use App\Controllers\Pages;
+
+$routes->match(['get', 'post'], 'guest/create', [Guest::class, 'create']);
+$routes->get('guest', [Guest::class, 'index']);
+
+$routes->match(['get', 'post'], 'news/create', [News::class, 'create']);
 $routes->get('news/(:segment)', [News::class, 'view']);
 $routes->get('news', [News::class, 'index']);
 $routes->get('pages', [Pages::class, 'index']);
--- ci4/app/Views/news/view.php
+++ ci4/app/Views/news/view.php
@@ -1,2 +1,3 @@
 <h2><?= esc($news['title']) ?></h2>
+</br>
 <p><?= esc($news['body']) ?></p>
